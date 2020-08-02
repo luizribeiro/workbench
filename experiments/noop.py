dd52c140e30a8b706de9c4b51d0b5f7fb94ba79b
Luiz Ribeiro <luizribeiro@gmail.com>
2020-08-02 17:45:56 -0400

    Simple noop experiment

---
+++ experiments/noop.py
@@ -0,0 +1,31 @@
+from dataclasses import dataclass
+
+from labctl.experiment import (
+    BaseInputParameters,
+    BaseOutputData,
+    Experiment,
+)
+
+
+@dataclass(frozen=True)
+class OutputData(BaseOutputData):
+    pass
+
+
+@dataclass(frozen=True)
+class InputParameters(BaseInputParameters):
+    pass
+
+
+class NoopExperiment(Experiment[InputParameters, OutputData]):
+    SAMPLING_RATE_IN_HZ: float = 1.0
+    DURATION_IN_SECONDS: float = 5.0
+
+    def start(self) -> None:
+        pass
+
+    def measure(self) -> OutputData:
+        return OutputData()
+
+    def stop(self) -> None:
+        pass
